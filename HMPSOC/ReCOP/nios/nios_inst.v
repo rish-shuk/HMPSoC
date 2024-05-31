864602dfff56fb177348547ecede0944c51e98a6
rshu284 <rish.shuk@gmail.com>
2024-05-31 13:19:32 +1200

    nios instantiation

---
+++ HMPSOC/ReCOP/nios/nios_inst.v
@@ -0,0 +1,14 @@
+	nios u0 (
+		.clk_clk                                  (<connected-to-clk_clk>),                                  //                               clk.clk
+		.reset_reset_n                            (<connected-to-reset_reset_n>),                            //                             reset.reset_n
+		.hex5_external_connection_export          (<connected-to-hex5_external_connection_export>),          //          hex5_external_connection.export
+		.hex4_external_connection_export          (<connected-to-hex4_external_connection_export>),          //          hex4_external_connection.export
+		.hex3_external_connection_export          (<connected-to-hex3_external_connection_export>),          //          hex3_external_connection.export
+		.hex2_external_connection_export          (<connected-to-hex2_external_connection_export>),          //          hex2_external_connection.export
+		.hex1_external_connection_export          (<connected-to-hex1_external_connection_export>),          //          hex1_external_connection.export
+		.pio_input_external_connection_export     (<connected-to-pio_input_external_connection_export>),     //     pio_input_external_connection.export
+		.recv_addr_pio_external_connection_export (<connected-to-recv_addr_pio_external_connection_export>), // recv_addr_pio_external_connection.export
+		.recv_data_pio_external_connection_export (<connected-to-recv_data_pio_external_connection_export>), // recv_data_pio_external_connection.export
+		.pk_input_external_connection_export      (<connected-to-pk_input_external_connection_export>)       //      pk_input_external_connection.export
+	);
+
