2be551f4cb6052d1f684324935f279a78f66d385
Ian Kuik <100252099+ikui753@users.noreply.github.com>
2024-06-02 16:43:47 +1200

    moved memory init files

--- HMPSOC/ReCOP/identifiers/identifier_mapping.py
+++ HMPSOC/ReCOP/identifiers/identifier_mapping.py
@@ -4,7 +4,7 @@ def read_txt_file(file_path):
 
 def write_mif_file(config_buttons, binary_identifiers, output_file):
     with open(output_file, 'w') as file:
-        file.write("DEPTH = {};\n".format(len(config_buttons) * 3))
+        file.write("DEPTH = {};\n".format(len(config_buttons) * 6))
         file.write("WIDTH = 16;\n")
         file.write("ADDRESS_RADIX = UNS;\n")
         file.write("DATA_RADIX = BIN;\n")
@@ -15,22 +15,26 @@ def write_mif_file(config_buttons, binary_identifiers, output_file):
             binary_identifier = binary_identifiers[i].strip()
             first_16_bits = binary_identifier[:16]
             last_16_bits = binary_identifier[16:]
-            file.write("{} : {};\n".format(3*i + 1, config_button))
-            file.write("{} : {};\n".format(3*i + 2, first_16_bits))
-            file.write("{} : {};\n".format(3*i + 3, last_16_bits))
+            file.write("{} : {};\n".format(6*i + 1, config_button))
+            file.write("{} : {};\n".format(6*i + 2, first_16_bits))
+            file.write("{} : {};\n".format(6*i + 3, last_16_bits))
+            file.write("{} : {};\n".format(6*i + 4, config_button))
+            file.write("{} : {};\n".format(6*i + 5, first_16_bits))
+            file.write("{} : {};\n".format(6*i + 6, last_16_bits))
+            
         file.write("END;\n")
 
 def main():
-    config_buttons_file = input("Enter the path to the config buttons file: ")
-    binary_identifiers_file = input("Enter the path to the binary identifiers file: ")
-    output_file = input("Enter the path to the output .mif file: ")
+    config_buttons_file = "configurations_min.txt"
+    binary_identifiers_file = "identifiers_min.txt"
+    output_file = "data_mem.mif"
 
     config_buttons = read_txt_file(config_buttons_file)
     binary_identifiers = read_txt_file(binary_identifiers_file)
 
-    if len(config_buttons) != len(binary_identifiers):
-        print("Error: The number of config buttons and binary identifiers must be the same.")
-        return
+    # if len(config_buttons) != len(binary_identifiers):
+    #     print("Error: The number of config buttons and binary identifiers must be the same.")
+    #     return
 
     write_mif_file(config_buttons, binary_identifiers, output_file)
     print("MIF file generated successfully.")
